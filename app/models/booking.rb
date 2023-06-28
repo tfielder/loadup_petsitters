ed65bafa253e84706d33548629f0861e73a5e1d0
Tim Fielder <timfielderdev@gmail.com>
2023-06-27 23:13:46 -0600

    add pricecalculator

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,9 @@
 class Booking < ApplicationRecord
+    include PriceCalculator
     enum animal_type: { dog: 0, cat: 1 }
+
+    def appointment_price
+        PriceCalculator.calculate_appointment_price(self.animal_type, self.hours_requested)
+        10
+    end
 end
