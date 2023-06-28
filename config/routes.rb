eecec896a78e8d8bfb4c901507c3158767374218
Tim Fielder <timfielderdev@gmail.com>
2023-06-28 12:48:27 -0600

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-  get 'admin/delete'
-  resource :bookings, only: [:create, :show, :update, :destroy]
+  root to: 'welcome#home'
+  get 'admin/bookings', to: 'admin#index'
+  
+  resources :bookings, except: [:index]
 
-  resources :admin do
-    resources :bookings, only: [:index]
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
