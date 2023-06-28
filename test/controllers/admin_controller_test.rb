b40c9e7fd6263601ec04e4f6e160348b24b5f638
Tim Fielder <timfielderdev@gmail.com>
2023-06-28 17:23:30 -0600

    update testing suite

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @bookings = []
+    @bookings << bookings(:one)
+    @bookings << bookings(:two)
+    @price_total = 12345
+  end
+  
   test "should get index" do
-    get admin_index_url
+    get admin_bookings_url
     assert_response :success
   end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get welcome_home_url
+  test "should get home page" do
+    get root_url
     assert_response :success
   end
 end
