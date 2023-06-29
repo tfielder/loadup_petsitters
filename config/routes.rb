33968a67e7f268c09f1c73822bacb30722f6b669
Tim Fielder <timfielderdev@gmail.com>
2023-06-28 18:11:30 -0600

    add testing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,4 @@ Rails.application.routes.draw do
   resources :bookings, except: [:index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
